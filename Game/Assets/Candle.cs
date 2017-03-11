d5420d5b68cf6a193300cc5e4da71811a7982555
Jorge Veiga de Souza Junior <jorgeveiga0@gmail.com>
2017-03-11 00:04:15 -0400

    Updates

---
+++ Game/Assets/Candle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Candle : MonoBehaviour {
+
+	SpriteRenderer alpha;
+	public Color flame;
+	public float intense;
+
+	// Use this for initialization
+	void Start () {
+		alpha = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		flame.a = (intense + Mathf.PingPong ((Time.time/2), Random.Range(0.01f, 0.05f)));
+		alpha.color = flame;
+	}
+
+}
