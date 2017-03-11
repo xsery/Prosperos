159a5112ee46f422f524a97c3e6db41f533d49b2
Jorge Veiga de Souza Junior <jorgeveiga0@gmail.com>
2017-03-11 10:14:42 -0400

    Intro animation, Corridor Dors, rain on windows, lightnings.

---
+++ Game/Assets/ChangeScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+
+	public Object scene;
+
+	void Start () {
+		StartCoroutine("ChangeToScene");
+	}
+
+	IEnumerator ChangeToScene(){
+		yield return new WaitForSeconds (7.5f);
+		SceneManager.LoadScene (scene.name);
+	}
+}
